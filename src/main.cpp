f1b641513f04ff12f87ae0391a3b91e2258f5c8b
Iman Alavi Fazel <im.alavifazel@gmail.com>
2018-09-12 11:49:19 -0400

    Added command line argument parsing feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,52 @@
 #include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <vector>
 #include "relay.h"
-using namespace std;
+#include <boost/program_options.hpp>
+#include <exception>
 
-int main() {
-  relay r1{"192.168.1.51"};
-  r1.off();
-  sleep(1000); 
-  r1.on();
-  cout << r1 << endl;
+namespace po = boost::program_options;
+
+using namespace std;
+using namespace boost;
+
+int main(int ac, char* av[]) {
+  try {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help", "produce help message")
+      ("address,a", po::value<string>(),
+       "relay's address")
+
+      ("turn,t", po::value<string>(),
+       "turn relay on and off")
+      ;
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(ac, av).
+	      options(desc).run(), vm);
+    po::notify(vm);
+    
+    if (vm.count("help")) {
+      cout << "Usage: options_description [options]\n";
+      cout << desc;
+      return 0;
+    }
+
+    if (vm.count("address")){
+      relay r1{vm["address"].as<string>()};
+      if( vm["turn"].as<string>() == "on") {
+	r1.on();
+      } else {
+	r1.off();
+      }
+    }
+
+  } catch(std::exception& e){
+    cout << e.what() << '\n';
+  }
+
 }
 
 
